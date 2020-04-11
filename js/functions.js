d71934d1035cebca3c36f9aca566586c44e83b86
metiks <35481766+metiks@users.noreply.github.com>
2020-04-11 17:16:21 +0700

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,3 @@
-
-
 function viewArticle(pdf_file, team_names, ppt_link){
 	parent.document.getElementById("pdf_load").src = pdf_file;
 	parent.document.getElementById("team_load").innerHTML = team_names;
@@ -11,6 +9,7 @@ function loadTeam(kelompok){
     team_rubrik = teamDesc[kelompok][1];
     team_lap_file = teamDesc[kelompok][2];
     team_ppt_file = teamDesc[kelompok][3];
+
     for (var i=0; i<=kelompok; i++) {
 		detailTeam = "<H3>PENULIS</H3>" + "<ul>";
 	 	jum_team = anggota[kelompok].length;
@@ -20,14 +19,12 @@ function loadTeam(kelompok){
 	 	}
 	 	detailTeam = detailTeam + "</ul>";
 	}
-	   
+	window.alert( teamDesc[kelompok][2]); 
+	window.alert( teamDesc[kelompok][3]);   
    	//file_loc = "materi/" + team_rubrik + "Kelompok" + kelompok + "-Laporan.pdf";
-   	file_loc = "https://drive.google.com/file/d/" + team_lap_file + "/preview";
-	ppt_loc =  "https://drive.google.com/file/d/" + team_ppt_file + "/preview";
+   	file_loc = "https://drive.google.com/file/d/" + teamDesc[kelompok][2] + "/preview";
+	ppt_loc =  "https://drive.google.com/file/d/" + teamDesc[kelompok][3] + "/preview";
 	ppt_link =  '<a href="'+ppt_loc+'" target="ppt">'+'File Presentasi'+'</a>';	
-	window.alert(team_lap_file);
-	window.alert(team_ppt_file);
-
   	return viewArticle( file_loc, detailTeam, ppt_link);
 }
 
